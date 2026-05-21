21a0411d29b11a59861979676e2b84ee0d00f9ae
b4130478-web <b4130478@gmail.com>
2026-05-21 21:21:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,9 @@
 module.exports = {
   port: process.env.PORT || 3000,
   mongodb_uri: process.env.MONGODB_URI || 'mongodb://localhost/mydb',
-  // Add other configuration options here
-}; 
-2  set up environment variable(.env) fill
-PORT=3000
-MONGODB_URI=mongodb://localhost/mydb
-SECRET_KEY=mysecretkey
+  
 
-## 3  create express app index.js and fill
+
 
 const express = require('express');
 const cors = require('cors');
