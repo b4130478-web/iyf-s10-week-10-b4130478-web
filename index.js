58a7c91b20e37a3ca190b08605f31c872f90c024
b4130478-web <b4130478@gmail.com>
2026-05-21 21:24:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-module.exports = {
-  port: process.env.PORT || 3000,
-  mongodb_uri: process.env.MONGODB_URI || 'mongodb://localhost/mydb',
-  
-
-
-
 const express = require('express');
 const cors = require('cors');
 const config = require('./config/config');
